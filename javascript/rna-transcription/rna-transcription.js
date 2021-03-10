56deab99b441417e6d7ecbc12d9fed6295561939
tabbathacrouch <tabbathacrouch@gmail.com>
2021-03-10 13:19:24 -0600

    updating rna-transcription exercise to include instructions;

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,20 +1,13 @@
-//
-// This is only a SKELETON file for the 'RNA Transcription' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-// *** 1st attempt ***
-// export const toRna = (dna) => {
-//   if (!STRANDS[dna] && dna.length < 1) {
-//     return "";
-//   } else if (dna.length === 1) {
-//     return STRANDS[dna];
-//   } else {
-//     var rna = dna.split("");
-//     const rnamap = rna.map((element) => STRANDS[element]);
-//     return rnamap.join("");
-//   }
-// };
+// exercism --> rna transcription
+// Given a DNA strand, return its RNA complement (per RNA transcription).
+// Both DNA and RNA strands are a sequence of nucleotides.
+// The four nucleotides found in DNA are adenine (A), cytosine (C), guanine (G) and thymine (T).
+// The four nucleotides found in RNA are adenine (A), cytosine (C), guanine (G) and uracil (U).
+// Given a DNA strand, its transcribed RNA strand is formed by replacing each nucleotide with its complement:
+// G -> C
+// C -> G
+// T -> A
+// A -> U
 
 export const toRna = (dna) => {
   if (!dna.length) {
@@ -30,3 +23,17 @@ export const toRna = (dna) => {
 };
 
 export const STRANDS = { G: "C", C: "G", T: "A", A: "U" };
+
+
+// -----1st attempt-----
+// export const toRna = (dna) => {
+//   if (!STRANDS[dna] && dna.length < 1) {
+//     return "";
+//   } else if (dna.length === 1) {
+//     return STRANDS[dna];
+//   } else {
+//     var rna = dna.split("");
+//     const rnamap = rna.map((element) => STRANDS[element]);
+//     return rnamap.join("");
+//   }
+// };
