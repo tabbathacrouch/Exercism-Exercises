e7d330ea7b196377285d973a625eee2f29d93a99
tabbathacrouch <tabbathacrouch@gmail.com>
2021-03-10 13:51:51 -0600

    update to include instructions and refactor solution

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -1,21 +1,54 @@
-// This is only a SKELETON file for the 'Collatz Conjecture' exercise. It's been provided as a
-// convenience to get you started writing code faster.
+// exercism --> collatz-conjecture
+// The Collatz Conjecture or 3x+1 problem can be summarized as follows:
+// Take any positive integer n. If n is even, divide n by 2 to get n / 2. If n is odd, multiply n by 3 and add 1 to get 3n + 1. 
+// Repeat the process indefinitely. The conjecture states that no matter which number you start with, you will always reach 1 eventually.
+// Given a number n, return the number of steps required to reach 1.
+// Examples
+// Starting with n = 12, the steps would be as follows:
+// 12
+// 6
+// 3
+// 10
+// 5
+// 16
+// 8
+// 4
+// 2
+// 1
+// Resulting in 9 steps. So for input n = 12, the return value would be 9.
 
-export const steps = (num) => {
-  let count = 0;
-  if (num === 1) {
-    return count;
-  }
-  if (num < 1) {
+export const steps = (n) => {
+  if (n < 1) {
     throw new Error("Only positive numbers are allowed");
   }
-  while (num !== 1) {
-    if (num % 2 === 0) {
-      num = num / 2;
+  let count = 0;
+  while (n > 1) {
+    if (n % 2 === 0) {
+      n = n / 2;
     } else {
-      num = num * 3 + 1;
+      n = n * 3 + 1;
     }
     count += 1;
   }
   return count;
 };
+
+// -----1st attempt----- (contains excess code)
+// export const steps = (num) => {
+//   let count = 0;
+//   if (num === 1) {
+//     return count;
+//   }
+//   if (num < 1) {
+//     throw new Error("Only positive numbers are allowed");
+//   }
+//   while (num !== 1) {
+//     if (num % 2 === 0) {
+//       num = num / 2;
+//     } else {
+//       num = num * 3 + 1;
+//     }
+//     count += 1;
+//   }
+//   return count;
+// };
