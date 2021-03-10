9508adea85718f00b1aa9f3d0b54c52b2495bed5
tabbathacrouch <tabbathacrouch@gmail.com>
2021-03-10 13:40:20 -0600

    update leap exercise to include instructions

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,7 +1,10 @@
-//
-// This is only a SKELETON file for the 'Leap' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+// exercism --> leap
+// Given a year, report if it is a leap year.
+// The tricky thing here is that a leap year in the Gregorian calendar occurs:
+// on every year that is evenly divisible by 4
+//   except every year that is evenly divisible by 100
+//     unless the year is also evenly divisible by 400
+// For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is.
 
 export const isLeap = (year) => {
   if (year % 400 === 0) {
