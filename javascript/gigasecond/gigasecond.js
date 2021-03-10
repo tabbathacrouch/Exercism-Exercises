ca9c76ddf6996487a96bb9d7d42d571c716950e8
tabbathacrouch <tabbathacrouch@gmail.com>
2021-03-10 13:15:58 -0600

    update gigasecond exercise to add instructions

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,10 +1,12 @@
-//
-// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+// exercism --> gigasecond
+// Given a moment, determine the moment that would be after a gigasecond has passed.
+// A gigasecond is 10^9 (1,000,000,000) seconds.
+// It is possible to return a correct value for this exercise by mutating the solution function argument. 
+// Although there are legitimate use cases for mutating function arguments, this is usually undesirable, and in the case of this exercise, clearly unexpected. 
+// For this reason, the test suite has a test that fails in case the argument has been modified after the function execution.
 
 export const gigasecond = (moment) => {
-  return new Date(moment.getTime()+ 10**9 * 1000);
+  return new Date(moment.getTime()+ 10 ** 9 * 1000);
 };
 
 
