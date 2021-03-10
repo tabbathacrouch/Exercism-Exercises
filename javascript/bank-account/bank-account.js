97edce85582eb46295f0b361b30950e293126b44
tabbathacrouch <tabbathacrouch@gmail.com>
2021-03-10 13:31:10 -0600

    update bank account exercise to include instructions

--- javascript/bank-account/bank-account.js
+++ javascript/bank-account/bank-account.js
@@ -1,6 +1,9 @@
-// This is only a SKELETON file for the 'Bank Account' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+// exercism --> bank account
+// Simulate a bank account supporting opening/closing, withdrawals, and deposits of money. Watch out for concurrent transactions!
+// A bank account can be accessed in multiple ways. Clients can make deposits and withdrawals using the internet, mobile phones, etc.
+// Shops can charge against the account.
+// Create an account that can be accessed from multiple threads/processes (terminology depends on your programming language).
+// It should be possible to close an account; operations against a closed account must fail.
 
 export class BankAccount {
   constructor() {
