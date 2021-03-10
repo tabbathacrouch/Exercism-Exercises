78021d9fd2f8292da980237868336d8c2af8fb2c
tabbathacrouch <tabbathacrouch@gmail.com>
2021-03-10 11:56:49 -0600

    updating two-fer to include instructions

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,13 +1,20 @@
-import { stringify } from "querystring";
-import { isNull } from "util";
+// exercism --> two-fer
+//Two-fer or 2-fer is short for two for one. One for you and one for me.
+// Given a name, return a string with the message:
+// One for name, one for me.
+// Where "name" is the given name.
+// However, if the name is missing, return the string:
+// One for you, one for me.
+// Here are some examples:
+// Name	String to return
+// Alice	One for Alice, one for me.
+// Bob	One for Bob, one for me.
+// One for you, one for me.
+// Zaphod	One for Zaphod, one for me.
 
-//
-// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-//
-//
-//*** 1st attempt ***
+export const twoFer = name => `One for ${name || "you"}, one for me.`;
+
+// ----- 1st attempt -----
 // export const twoFer = (name) => {
 //   if (!name){name = 'you'};
 //   let str1 = 'One for ';
@@ -15,6 +22,3 @@ import { isNull } from "util";
 //   let str3 =  str1 + name + str2;
 //   return str3;  
 // };
-
-
-export const twoFer = name => `One for ${name || "you"}, one for me.`;
