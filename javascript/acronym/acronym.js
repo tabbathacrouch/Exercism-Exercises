693cb44cd7c76fc014d75c8205654229bcf428b5
tabbathacrouch <tabbathacrouch@gmail.com>
2021-03-10 17:00:02 -0600

    added instructions to acronym exercise

--- javascript/acronym/acronym.js
+++ javascript/acronym/acronym.js
@@ -1,4 +1,7 @@
 // exercism --> acronym
+// Convert a phrase to its acronym.
+// Techies love their TLA (Three Letter Acronyms)!
+// Help generate some jargon by writing a program that converts a long name like Portable Network Graphics to its acronym (PNG).
 
 export const parse = (string) =>
   string
