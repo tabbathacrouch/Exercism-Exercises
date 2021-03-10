7ee5c07f81569c75a534cb3aa65f6db9d9fe9ebf
tabbathacrouch <tabbathacrouch@gmail.com>
2021-03-10 17:04:37 -0600

    added instructions to isogram exercise

--- javascript/isogram/isogram.js
+++ javascript/isogram/isogram.js
@@ -1,5 +1,23 @@
 // exercism.io --> isogram
+// Determine if a word or phrase is an isogram.
+// An isogram (also known as a "nonpattern word") is a word or phrase without a repeating letter, however spaces and hyphens are allowed to appear multiple times.
+// Examples of isograms:
+// lumberjacks
+// background
+// downstream
+// six-year-old
+// The word isograms, however, is not an isogram, because the s repeats.
 
+export const isIsogram = (string) => {
+  const iso = string
+    .toLowerCase()
+    .split("")
+    .filter((c) => ![" ", "-"].includes(c));
+
+  return new Set(iso).size === iso.length;
+};
+
+// -----1st attempt-----
 // export const isIsogram = (string) => {
 //   if (string.length === 0) {
 //     return true;
@@ -16,12 +34,3 @@
 //   }
 //   return false;
 // };
-
-export const isIsogram = (string) => {
-  const iso = string
-    .toLowerCase()
-    .split("")
-    .filter((c) => ![" ", "-"].includes(c));
-
-  return new Set(iso).size === iso.length;
-};
