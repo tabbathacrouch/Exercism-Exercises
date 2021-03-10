8200a9e57cd85f203c6d61a99720440e9c4fd32a
tabbathacrouch <tabbathacrouch@gmail.com>
2021-03-10 17:02:07 -0600

    added instructions to high-scores exercise

--- javascript/high-scores/high-scores.js
+++ javascript/high-scores/high-scores.js
@@ -1,7 +1,7 @@
-//
-// This is only a SKELETON file for the 'High Scores' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+// exercism --> high scores
+// Manage a game player's High Score list.
+// Your task is to build a high-score component of the classic Frogger game, one of the highest selling and addictive games of all time, and a classic of the arcade era. 
+// Your task is to write methods that return the highest score from the list, the last added score and the three highest scores.
 
 export class HighScores {
   constructor([...scores]) {
