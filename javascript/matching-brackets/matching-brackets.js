ea61dbdcb2c2503b83759f588cb8a6f844118015
tabbathacrouch <tabbathacrouch@gmail.com>
2021-03-11 17:29:52 -0600

    added instructions, refactored solution

--- javascript/matching-brackets/matching-brackets.js
+++ javascript/matching-brackets/matching-brackets.js
@@ -1,26 +1,49 @@
 // exercism --> matching-brackets
+// Given a string containing brackets [], braces {}, parentheses (), or any combination thereof, verify that any and all pairs are matched and nested correctly.
 
-export const isPaired = (input) => {
-  const brackets = [];
+export const isPaired = (str) => {
   const pairedSymbols = {
     "{": "}",
     "[": "]",
     "(": ")",
   };
-  const closedSymbols = {
-    "}": true,
-    "]": true,
-    ")": true,
-  };
-  for (let i = 0; i < input.length; i++) {
-    let character = input[i];
-    if (pairedSymbols[character]) {
-      brackets.push(character);
-    } else if (closedSymbols[character]) {
-      if (pairedSymbols[brackets.pop()] !== character) {
+  const brackets = [...str].filter((x) => [..."({[]})"].includes(x));
+  const stack = [];
+  for (let symbol of brackets) {
+    if ("([{".includes(symbol)) {
+      stack.push(symbol);
+    }
+    if (")]}".includes(symbol)) {
+      if (!(pairedSymbols[stack.pop()] === symbol)) {
         return false;
       }
     }
   }
-  return brackets.length === 0;
+  return stack.length > 0 ? false : true;
 };
+
+// -----1st attempt-----
+// export const isPaired = (input) => {
+//   const brackets = [];
+//   const pairedSymbols = {
+//     "{": "}",
+//     "[": "]",
+//     "(": ")",
+//   };
+//   const closedSymbols = {
+//     "}": true,
+//     "]": true,
+//     ")": true,
+//   };
+//   for (let i = 0; i < input.length; i++) {
+//     let character = input[i];
+//     if (pairedSymbols[character]) {
+//       brackets.push(character);
+//     } else if (closedSymbols[character]) {
+//       if (pairedSymbols[brackets.pop()] !== character) {
+//         return false;
+//       }
+//     }
+//   }
+//   return brackets.length === 0;
+// };
