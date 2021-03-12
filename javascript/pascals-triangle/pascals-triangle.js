5cda110fe3c413ca5c03eb21f11d75e5aca12f24
tabbathacrouch <tabbathacrouch@gmail.com>
2021-03-12 14:42:41 -0600

    pascals-triangle exercise

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -1,4 +1,12 @@
 // exercism --> pascals-triangle
+// Compute Pascal's triangle up to a given number of rows.
+// In Pascal's Triangle each number is computed by adding the numbers to the right and left of the current position in the previous row.
+//     1
+//    1 1
+//   1 2 1
+//  1 3 3 1
+// 1 4 6 4 1
+// # ... etc
 
 export const rows = (n) => {
   const pascalsTriangle = [[1], [1, 1]];
