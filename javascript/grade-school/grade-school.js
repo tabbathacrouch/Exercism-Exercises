079558dec2290eaebe989e49d32ed8b26e1508e7
tabbathacrouch <tabbathacrouch@gmail.com>
2021-03-14 16:08:28 -0500

    grade school exercise

---
+++ javascript/grade-school/grade-school.js
@@ -0,0 +1,38 @@
+// exercism --> grade school
+// Given students' names along with the grade that they are in, create a roster for the school.
+// In the end, you should be able to:
+// Add a student's name to the roster for a grade
+// "Add Jim to grade 2."
+// "OK."
+// Get a list of all students enrolled in a grade
+// "Which students are in grade 2?"
+// "We've only got Jim just now."
+// Get a sorted list of all students in all grades. Grades should sort as 1, 2, 3, etc., and students within a grade should be sorted alphabetically by name.
+// "Who all is enrolled in school right now?"
+// "Let me think. We have Anna, Barb, and Charlie in grade 1, Alex, Peter, and Zoe in grade 2 and Jim in grade 5. So the answer is: Anna, Barb, Charlie, Alex, Peter, Zoe and Jim"
+// Note that all our students only have one name. (It's a small town, what do you want?)
+// For bonus points
+// Did you get the tests passing and the code clean? If you want to, these are some additional things you could try:
+// If you're working in a language with mutable data structures and your implementation allows outside code to mutate the school's internal DB directly, see if you can prevent this. Feel free to introduce additional tests.
+// Then please share your thoughts in a comment on the submission. Did this experiment make the code better? Worse? Did you learn anything from it?
+
+export class GradeSchool {
+  constructor() {
+    this._roster = {};
+  }
+
+  roster() {
+    return JSON.parse(JSON.stringify(this._roster));
+  }
+
+  add(name, grade) {
+    let currentStudents = this._roster[grade] || [];
+    Object.values(this._roster).filter(x => x.indexOf(name) !== -1).forEach(x => x.splice(x.indexOf(name),1));
+    currentStudents.push(name);
+    this._roster[grade] = currentStudents.sort();
+  }
+
+  grade(grade) {
+    return JSON.parse(JSON.stringify(this._roster[grade] || []));
+  }
+}
