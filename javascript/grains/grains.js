ab51d7bab7599597103b5515cc440cea206a1a93
tabbathacrouch <tabbathacrouch@gmail.com>
2021-03-21 18:17:11 -0500

    alternative to left shift operator

--- javascript/grains/grains.js
+++ javascript/grains/grains.js
@@ -10,7 +10,8 @@ export const square = (num) => {
   if (num < 1 || num > 64) {
     throw new Error("square must be between 1 and 64");
   }
-  return 1n << BigInt(num - 1);
+  return BigInt(2 ** (num -1));
+  // return 1n << BigInt(num - 1);
 };
 
 export const total = () => {
