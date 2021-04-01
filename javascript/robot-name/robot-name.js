90682d1346d9db40bfbdeed1c0fdd07fd7c24d8e
tabbathacrouch <tabbathacrouch@gmail.com>
2021-04-01 14:52:33 -0500

    robot name exercise

---
+++ javascript/robot-name/robot-name.js
@@ -0,0 +1,43 @@
+// exercism --> robot name
+// Manage robot factory settings.
+// When a robot comes off the factory floor, it has no name.
+// The first time you turn on a robot, a random name is generated in the format of two uppercase letters followed by three digits, such as RX837 or BC811.
+// Every once in a while we need to reset a robot to its factory settings, which means that its name gets wiped. The next time you ask, that robot will respond with a new random name.
+// The names must be random: they should not follow a predictable sequence. Using random names means a risk of collisions. Your solution must ensure that every existing robot has a unique name.
+
+export class Robot {
+  static robotNames = [];
+
+  constructor() {
+    this.generateName();
+  }
+
+  get name() {
+    return this.robotName;
+  }
+
+  generateName() {
+    let robotName = "";
+    for (let i = 0; i < 2; i++) {
+      robotName += String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    }
+    for (let i = 0; i < 3; i++) {
+      robotName += Math.floor(Math.random() * 10);
+    }
+    if (Robot.robotNames.indexOf(robotName) === -1) {
+      Robot.robotNames.push(robotName);
+      this.robotName = robotName;
+    } else {
+      this.generateName();
+    }
+    return;
+  }
+
+  reset() {
+    this.robotName = "";
+    this.generateName();
+    return;
+  }
+}
+
+Robot.releaseNames = () => {};
