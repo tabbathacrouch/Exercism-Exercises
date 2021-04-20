1b86cb251facb2a4c28e8644f37975d1f98aa628
tabbathacrouch <tabbathacrouch@gmail.com>
2021-04-20 11:56:51 -0500

    kindergarten-garden exercise

---
+++ javascript/kindergarten-garden/kindergarten-garden.js
@@ -0,0 +1,64 @@
+// exercism --> kindergarten-garden
+// Given a diagram, determine which plants each child in the kindergarten class is responsible for.
+// The kindergarten class is learning about growing plants. The teacher thought it would be a good idea to give them actual seeds, plant them in actual dirt, and grow actual plants.
+// They've chosen to grow grass, clover, radishes, and violets.
+// To this end, the children have put little cups along the window sills, and planted one type of plant in each cup, choosing randomly from the available types of seeds.
+// [window][window][window]
+// ........................ # each dot represents a cup
+// ........................
+// There are 12 children in the class:
+// Alice, Bob, Charlie, David,
+// Eve, Fred, Ginny, Harriet,
+// Ileana, Joseph, Kincaid, and Larry.
+// Each child gets 4 cups, two on each row. Their teacher assigns cups to the children alphabetically by their names.
+// The following diagram represents Alice's plants:
+// [window][window][window]
+// VR......................
+// RG......................
+// In the first row, nearest the windows, she has a violet and a radish. In the second row she has a radish and some grass.
+// Your program will be given the plants from left-to-right starting with the row nearest the windows. From this, it should be able to determine which plants belong to each student.
+// For example, if it's told that the garden looks like so:
+// [window][window][window]
+// VRCGVVRVCGGCCGVRGCVCGCGV
+// VRCCCGCRRGVCGCRVVCVGCGCV
+// Then if asked for Alice's plants, it should provide:
+// Violets, radishes, violets, radishes
+// While asking for Bob's plants would yield:
+// Clover, grass, clover, clover
+
+const DEFAULT_STUDENTS = [
+  "Alice",
+  "Bob",
+  "Charlie",
+  "David",
+  "Eve",
+  "Fred",
+  "Ginny",
+  "Harriet",
+  "Ileana",
+  "Joseph",
+  "Kincaid",
+  "Larry",
+];
+
+const PLANT_CODES = {
+  G: "grass",
+  V: "violets",
+  R: "radishes",
+  C: "clover",
+};
+
+export class Garden {
+  constructor(diagram, students = DEFAULT_STUDENTS) {
+    this.rows = diagram.split("\n");
+    this.students = students.sort();
+  }
+
+  plants(student) {
+    const studentIndex = this.students.indexOf(student);
+    const studentPlantCodes = this.rows.map(
+      (x) => x[studentIndex * 2] + x[studentIndex * 2 + 1]
+    ).map((x) => x.split("")).flat();
+   return studentPlantCodes.map(x => PLANT_CODES[x]);
+  }
+}
